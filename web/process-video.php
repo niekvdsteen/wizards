5f048c1f3c10fd246a5ca44bcf9fb04a6f6d60e3
Niek van der Steen <niek@camtronics.nl>
2024-05-05 03:54:51 +0200

    v2

--- web/process-video.php
+++ web/process-video.php
@@ -2,11 +2,10 @@
 
 $video_id = $_POST["id"];
 
-//shell_exec("/usr/home/bruhnc/ffmpeg -y -i uploads/$video_id .mp4 uploads/$video_id-0.mp4 > /dev/null 2> ffmpeg.log");
-//shell_exec("/usr/home/bruhnc/ffmpeg -y -i uploads/$video_id.mp4 uploads/$video_id-0.mp4 > /dev/null 2> ffmpeg.log");
-//shell_exec("/usr/home/bruhnc/ffmpeg -y -i uploads/$video_id.mp4 uploads/$video_id-0.mp4");
-shell_exec("/usr/bin/ffmpeg -y -i uploads/$video_id.mp4 -c copy -map 0 -movflags +faststart uploads/$video_id-0.mp4 > /dev/null 2> ffmpeg.log");
+if(!preg_match('/^[a-zA-Z0-9]+[a-zA-Z0-9._]+$/', $video_id))
+die();
 
+shell_exec("/usr/bin/ffmpeg -y -i uploads/$video_id.mp4 -c copy -map 0 -movflags +faststart uploads/$video_id-0.mp4 > /dev/null 2> ffmpeg.log");
 
 // Load video page
 $videoPage = file_get_contents("clips/$video_id.html");
